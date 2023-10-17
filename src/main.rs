407cf97ea3d2a8d196d7bc8ffdb87d9526556e14
Forrest Walker <forrestpatwalker@hotmail.com>
2023-10-17 11:24:11 -0600

    add axum and shuttle logic for basic web app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, response::IntoResponse, routing::get};
+
+#[shuttle_runtime::main]
+async fn shuttle() -> shuttle_axum::ShuttleAxum {
+    let app = Router::new().route("/", get(hello_world));
+    Ok(app.into())
+}
+
+async fn hello_world() -> impl IntoResponse {
+    "Hello world!"
 }
